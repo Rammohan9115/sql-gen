ef13722543ea5c64b919b721cb2cebeeb4aa1898
Rammohan9115 <rammohan.rameez@gmail.com>
2023-11-01 20:23:37 -0400

    second

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import Form from '../components/Form';
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-indigo-400 via-indigo-200 to-pink-200 min-h-screen flex flex-col justify-center items-center text-white">
-      <div className="bg-white bg-opacity-70 p-4 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4 text-black">SQL Learning Dashboard</h1>
+    <div className="bg-gradient-to-r from-indigo-300 via-indigo-200 to-pink-200 min-h-screen flex flex-col justify-center items-center text-white relative">
+      <h1 className="text-center text-3xl font-bold text-black bg-gradient-to-r from-indigo-300 via-indigo-200 to-pink-200 py-2 px-4 w-full fixed top-0 left-0 z-10">SQL Learning Dashboard</h1>
+      <div className="mt-16 mb-30"> {/* Adjust the top margin to push content below the header */}
         <Form />
       </div>
     </div>
   );
 }
+
